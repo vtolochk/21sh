17483b5026cac397ff9b343b31f27d0d3f6a7d75
Valentin TOLOCHKO <vtolochk@e1r5p5.unit.ua>
2018-05-11 18:01:52 +0300

    continue

--- srcs/shell_loop.c
+++ srcs/shell_loop.c
@@ -12,18 +12,48 @@
 
 #include "shell.h"
 
-void shell_loop(void)
+void	set_raw_mode(struct termios *old_tty)
+{
+	struct termios tty;
+
+	tgetent(NULL, getenv("TERM"));
+	tcgetattr(0, &tty);
+	*old_tty = tty;
+	tty.c_lflag &= ECHO;
+	tty.c_lflag &= ICANON;
+	//tty.c_lflag &= ECHOCTL;
+	tty.c_cc[VMIN] = 1;
+	tcsetattr(0, TCSAFLUSH, &tty);
+}
+
+int read_command(void)
 {
 	char key[8];
+	//char buf[4096];
+	read(0, key, 8);
+//	if (ret == 8)
+//	{
+//
+//		return (0);
+//	}
+	//ft_printf("ret: %d, %s\n", ret, key);
+	if (key[0] == 'q')
+		return (1);
+	if (key[0] == 'p')
+		print_env();
+	return 0;
+}
 
+void shell_loop(void)
+{
+	struct termios old_tty;
+	set_raw_mode(&old_tty);
 	while (1)
 	{
 		signals();
 		print_prompt();
-		read(0, key, 8);
-		if (key[0] == 'q')
+		if (read_command() == 1)
 			break;
-		if (key[0] == 'p')
-			print_env();
 	}
+	tcsetattr(0, TCSAFLUSH, &old_tty);
 }
